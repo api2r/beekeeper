8159d36a9d025f887d5a57198bbc1c7571592728
Jon Harmon <jonthegeek@gmail.com>
2025-01-27 06:48:36 -0600

    Handle user agent purely in nectar.

--- R/generate_pkg-call.R
+++ R/generate_pkg-call.R
@@ -1,19 +1,18 @@
-.generate_call <- function(config, api_definition, pkg_agent, security_data) {
+.generate_call <- function(config, api_definition, security_data) {
   touched_files <- c(
-    .generate_call_r(config, api_definition, pkg_agent, security_data),
+    .generate_call_r(config, api_definition, security_data),
     .generate_call_test(config$api_abbr)
   )
   return(touched_files)
 }
 
-.generate_call_r <- function(config, api_definition, pkg_agent, security_data) {
+.generate_call_r <- function(config, api_definition, security_data) {
   .bk_use_template(
     template = "010-call.R",
     data = list(
       api_title = config$api_title,
       api_abbr = config$api_abbr,
       base_url = api_definition@servers@url,
-      pkg_agent = pkg_agent,
       has_security = security_data$has_security,
       security_arg_helps = security_data$security_arg_helps,
       security_signature = security_data$security_signature,
--- R/generate_pkg_main.R
+++ R/generate_pkg_main.R
@@ -5,14 +5,11 @@
 #' an opinionated framework for API packages.
 #'
 #' @param config_file The path to a beekeeper yaml file.
-#' @param pkg_agent A string to identify this package, for use in the
-#'   `user_agent` argument of [nectar::req_setup()].
 #'
 #' @return A character vector of paths to files that were added or updated,
 #'   invisibly.
 #' @export
-generate_pkg <- function(config_file = "_beekeeper.yml",
-                         pkg_agent = generate_pkg_agent(config_file)) {
+generate_pkg <- function(config_file = "_beekeeper.yml") {
   # TODO: Confirm that they use github & everything is committed. Error or warn
   # if not, letting them know that this can be destructive. Skip this check in
   # tests.
@@ -20,16 +17,16 @@ generate_pkg <- function(config_file = "_beekeeper.yml",
   config <- .read_config(config_file)
   api_definition <- .read_api_definition(config_file, config$rapid_file)
   .prepare_r()
-  touched_files <- .generate_pkg_impl(config, api_definition, pkg_agent)
+  touched_files <- .generate_pkg_impl(config, api_definition)
   return(invisible(touched_files))
 }
 
-.generate_pkg_impl <- function(config, api_definition, pkg_agent) {
+.generate_pkg_impl <- function(config, api_definition) {
   security_data <- .generate_security(
     config$api_abbr,
     api_definition@components@security_schemes
   )
-  call_files <- .generate_call(config, api_definition, pkg_agent, security_data)
+  call_files <- .generate_call(config, api_definition, security_data)
   path_files <- .generate_paths(
     api_definition@paths,
     config$api_abbr,
