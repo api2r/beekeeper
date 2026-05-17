dc3673388f7627de908633f235194f94a44b6727
Jon Harmon <jonthegeek@gmail.com>
2026-05-17 07:16:51 -0500

    Clean up and rearrange

--- tests/testthat/_snaps/generate_pkg-setup.md
+++ tests/testthat/_snaps/generate_pkg-setup.md
@@ -7,24 +7,3 @@
       ! Can't generate package files outside of a package.
       TMPDIR is not inside a package.
 
-# read_config() reads configs
-
-    Code
-      config
-    Output
-      $api_title
-      [1] "APIs.guru"
-      
-      $api_abbr
-      [1] "guru"
-      
-      $api_version
-      [1] "2.2.0"
-      
-      $rapid_file
-      [1] "_beekeeper_rapid.rds"
-      
-      $updated_on
-      [1] "2026-05-12 07:57:02 UTC"
-      
-
--- tests/testthat/test-generate_pkg-setup.R
+++ tests/testthat/test-generate_pkg-setup.R
@@ -13,21 +13,6 @@ test_that(".assert_is_pkg() isn't obtrusive for packages", {
   })
 })
 
-test_that("read_config() reads configs", {
-  config <- read_config(pkg_dir = test_path("_fixtures", "guru"))
-  expect_s3_class(config$updated_on, c("POSIXlt", "POSIXt"))
-  expect_snapshot({
-    config
-  })
-})
-
-test_that("read_api_definition() reads api_definitions", {
-  api_definition <- read_api_definition(
-    pkg_dir = test_path("_fixtures", "guru")
-  )
-  expect_s7_class(api_definition, rapid::class_rapid)
-})
-
 test_that(".setup_r() sets up dependencies (#16)", {
   skip_on_cran()
 
