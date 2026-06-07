cf90f9513e304b221a9e34a927e0f1342fdf17b6
Jon Harmon <jonthegeek@gmail.com>
2026-06-07 09:54:00 -0500

    Update prepare tests (#122)
    httr2 added a field to requests, so our expectation broke. Simplify that; we'll just look for the special stuff we touch.

--- tests/testthat/_fixtures/guru/testthat/test-010-prepare.R
+++ tests/testthat/_fixtures/guru/testthat/test-010-prepare.R
@@ -1,12 +1,6 @@
 test_that("Can prepare a request without errors", {
   test_result <- expect_no_error(guru_req_prepare("testing"))
   expect_s3_class(test_result, c("nectar_request", "httr2_request"))
-  expect_named(
-    test_result,
-    c("url", "method", "headers", "body", "fields", "options", "policies")
-  )
-  expect_contains(
-    names(test_result$policies),
-    "resp_tidy"
-  )
+  expect_contains(names(test_result), "policies")
+  expect_contains(names(test_result$policies), "resp_tidy")
 })
